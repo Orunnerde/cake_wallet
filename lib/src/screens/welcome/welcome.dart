573d90f4e852544791b862a026554592795fa337
Oleksandr Sobol <dr.alexander.sobol@gmail.com>
2019-08-05 21:34:24 +0300

    CWA-4 | Added styles of widgets to layout

--- lib/src/screens/welcome/welcome.dart
+++ lib/src/screens/welcome/welcome.dart
@@ -6,26 +6,27 @@ class Welcome extends StatelessWidget {
 
     // Welcome image
 
-    var _image = new Image(image: AssetImage('lib/src/screens/welcome/images/welcomeImg.png'));
+    var _image = new Image(image: AssetImage('assets/welcomeImg.png'));
 
     // Scales of widgets
 
     const double _imageScale = 0.4;
-    const double _firstTextScale = 0.13;
+    const double _firstTextScale = 0.12;
     const double _secondTextScale = 0.1;
     const double _thirdTextScale = 0.07;
-    const double _buttonScale = 0.07;
+    const double _buttonScale = 0.1;
     const double _buttonTextScale = 0.03;
     const double _marginScale = 0.02;
 
     // Values of insets and radius
 
-    const double _insets = 10.0;
+    const double _verticalInsets = 10.0;
+    const double _horizontalInsets = 30.0;
     const double _radius = 10.0;
 
     // Defining of screen height
 
-    double _screenHeight = MediaQuery.of(context).size.height - 2*_insets;
+    double _screenHeight = MediaQuery.of(context).size.height - 2*_verticalInsets;
 
     // Heights of widgets
 
@@ -39,13 +40,20 @@ class Welcome extends StatelessWidget {
 
     // Colors of widgets
 
-    Color _textColor = Colors.indigo[300];
+    Color _textColor = Color.fromARGB(255, 126, 147, 177);
     Color _buttonCreateColor = Colors.purple[50];
+    Color _borderButtonCreateColor = Colors.purple[100];
     Color _buttonRestoreColor = Colors.indigo[50];
+    Color _borderButtonRestoreColor = Colors.indigo[100];
 
     return Scaffold(
       body: Container(
-        padding: EdgeInsets.all(_insets),
+        padding: EdgeInsets.only(
+          left: _horizontalInsets,
+          top: _verticalInsets,
+          right: _horizontalInsets,
+          bottom: _verticalInsets
+        ),
         child: Column(
           children: <Widget>[
             Container(
@@ -56,15 +64,16 @@ class Welcome extends StatelessWidget {
                 child: _image,
               ),
             ),
-            SizedText('WELCOME\n TO CAKE WALLET', _firstTextHeight,
+            SizedText('WELCOME\nTO CAKE WALLET', _firstTextHeight,
+              fontWeight: FontWeight.bold,
               marginTop: _marginHeight,
               marginBottom: _marginHeight,
             ),
-            SizedText('Awesome wallet\n for Monero', _secondTextHeight,
+            SizedText('Awesome wallet\nfor Monero', _secondTextHeight,
               textColor: _textColor,
-              marginBottom: _marginHeight,
+              fontWeight: FontWeight.bold,
             ),
-            SizedText('Please make a selection below to either create \n a new wallet or restore a wallet', _thirdTextHeight,
+            SizedText('Please make a selection below to either create\na new wallet or restore a wallet', _thirdTextHeight,
               textColor: _textColor,
               marginTop: _marginHeight,
               marginBottom: _marginHeight,
@@ -72,13 +81,14 @@ class Welcome extends StatelessWidget {
             ButtonTheme(
               minWidth: double.infinity,
               height: _buttonHeight,
-              buttonColor: _buttonCreateColor,
-              child: RaisedButton(
+              child: FlatButton(
                 onPressed: (){},
-                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(_radius)),
+                color: _buttonCreateColor,
+                shape: RoundedRectangleBorder(side: BorderSide(color: _borderButtonCreateColor), borderRadius: BorderRadius.circular(_radius)),
                 child: Text('Create new',
                   style: TextStyle(
-                    fontSize: _buttonTextFontSize
+                    fontSize: _buttonTextFontSize,
+                    fontWeight: FontWeight.normal
                   ),
                 ),
               ),
@@ -89,13 +99,14 @@ class Welcome extends StatelessWidget {
             ButtonTheme(
               minWidth: double.infinity,
               height: _buttonHeight,
-              buttonColor: _buttonRestoreColor,
-              child: RaisedButton(
+              child: FlatButton(
                 onPressed: (){},
-                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(_radius)),
+                color: _buttonRestoreColor,
+                shape: RoundedRectangleBorder(side: BorderSide(color: _borderButtonRestoreColor), borderRadius: BorderRadius.circular(_radius)),
                 child: Text('Restore',
                   style: TextStyle(
-                    fontSize: _buttonTextFontSize
+                    fontSize: _buttonTextFontSize,
+                    fontWeight: FontWeight.normal
                   ),
                 ),
               ),
@@ -112,12 +123,15 @@ class SizedText extends StatelessWidget{
   final String _text;
   final double _heightContainer;
   Color _textColor;
+  FontWeight _fontWeight;
   double _marginTop, _marginBottom;
 
   SizedText(this._text, this._heightContainer, {Color textColor = Colors.black,
+    FontWeight fontWeight = FontWeight.normal,
     double marginTop = 0.0, double marginBottom = 0.0}){
 
     _textColor = textColor;
+    _fontWeight = fontWeight;
     _marginTop = marginTop;
     _marginBottom = marginBottom;
 
@@ -132,7 +146,7 @@ class SizedText extends StatelessWidget{
       child: FittedBox(
         fit: BoxFit.contain,
         child: Text(_text,
-          style: TextStyle(color: _textColor),
+          style: TextStyle(color: _textColor, fontWeight: _fontWeight),
           textAlign: TextAlign.center,
         ),
       ),
