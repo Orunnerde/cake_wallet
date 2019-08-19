165b9637b2867395fe58a7d7051ca50a6bd4b36b
Oleksandr Sobol <dr.alexander.sobol@gmail.com>
2019-08-19 12:52:21 +0300

    CWA-7 | Fixed overflow and added numeric keyboard

--- lib/src/screens/restore/restore_from_seed.dart
+++ lib/src/screens/restore/restore_from_seed.dart
@@ -55,132 +55,142 @@ class _RestoreFromSeedState extends State<RestoreFromSeed>{
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  Flexible(
-                      flex: 1,
-                      child: Container(
-                        padding: EdgeInsets.only(
-                            top: 20.0,
-                            bottom: 20.0
-                        ),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: <Widget>[
-                            TextFormField(
-                              decoration: InputDecoration(
-                                  hintStyle: TextStyle(color: Palette.lightBlue),
-                                  hintText: 'Wallet name',
-                                  focusedBorder: UnderlineInputBorder(
-                                      borderSide: BorderSide(
-                                          color: Palette.lightGrey,
-                                          width: 2.0
-                                      )
-                                  ),
-                                  enabledBorder: UnderlineInputBorder(
-                                      borderSide: BorderSide(
-                                          color: Palette.lightGrey,
-                                          width: 2.0
-                                      )
-                                  )
-                              ),
-                              validator: (value){
-                                return null;
-                              },
+                  Row(
+                    children: <Widget>[
+                      Flexible(
+                        child: Container(
+                          padding: EdgeInsets.only(top: 20.0),
+                          child: TextFormField(
+                            decoration: InputDecoration(
+                                hintStyle: TextStyle(color: Palette.lightBlue),
+                                hintText: 'Wallet name',
+                                focusedBorder: UnderlineInputBorder(
+                                    borderSide: BorderSide(
+                                        color: Palette.lightGrey,
+                                        width: 2.0
+                                    )
+                                ),
+                                enabledBorder: UnderlineInputBorder(
+                                    borderSide: BorderSide(
+                                        color: Palette.lightGrey,
+                                        width: 2.0
+                                    )
+                                )
                             ),
-                            TextFormField(
-                              decoration: InputDecoration(
-                                  hintStyle: TextStyle(color: Palette.lightBlue),
-                                  hintText: 'Restore from blockheight',
-                                  focusedBorder: UnderlineInputBorder(
-                                      borderSide: BorderSide(
-                                          color: Palette.lightGrey,
-                                          width: 2.0
-                                      )
-                                  ),
-                                  enabledBorder: UnderlineInputBorder(
-                                      borderSide: BorderSide(
-                                          color: Palette.lightGrey,
-                                          width: 2.0
-                                      )
-                                  )
-                              ),
-                              validator: (value){
-                                return null;
-                              },
-                            ),
-                          ],
-                        ),
+                            validator: (value){
+                              return null;
+                            },
+                          ),
+                        )
                       )
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Flexible(
+                        child: Container(
+                          padding: EdgeInsets.only(top: 20.0, bottom: 10.0),
+                          child: TextFormField(
+                            keyboardType: TextInputType.numberWithOptions(
+                                signed: false,
+                                decimal: false
+                            ),
+                            decoration: InputDecoration(
+                                hintStyle: TextStyle(color: Palette.lightBlue),
+                                hintText: 'Restore from blockheight',
+                                focusedBorder: UnderlineInputBorder(
+                                    borderSide: BorderSide(
+                                        color: Palette.lightGrey,
+                                        width: 2.0
+                                    )
+                                ),
+                                enabledBorder: UnderlineInputBorder(
+                                    borderSide: BorderSide(
+                                        color: Palette.lightGrey,
+                                        width: 2.0
+                                    )
+                                )
+                            ),
+                            validator: (value){
+                              return null;
+                            },
+                          ),
+                        )
+                      )
+                    ],
                   ),
                   Text(
                     'or',
                     style: TextStyle(fontSize: 16.0),
                   ),
-                  Flexible(
-                      flex: 1,
-                      child: Container(
-                        padding: EdgeInsets.only(
-                            top: 20.0,
-                            bottom: 20.0
-                        ),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: <Widget>[
-                            InkWell(
-                              onTap: (){
-                                _selectDate(context);
-                              },
-                              child: IgnorePointer(
-                                child: TextFormField(
-                                  decoration: InputDecoration(
-                                      hintStyle: TextStyle(color: Palette.lightBlue),
-                                      hintText: 'Restore from date',
-                                      focusedBorder: UnderlineInputBorder(
-                                          borderSide: BorderSide(
-                                              color: Palette.lightGrey,
-                                              width: 2.0
-                                          )
-                                      ),
-                                      enabledBorder: UnderlineInputBorder(
-                                          borderSide: BorderSide(
-                                              color: Palette.lightGrey,
-                                              width: 2.0
-                                          )
-                                      )
-                                  ),
-                                  controller: _controller,
-                                  validator: (value){
-                                    return null;
-                                  },
+                  Row(
+                    children: <Widget>[
+                      Flexible(
+                        child: Container(
+                          child: InkWell(
+                            onTap: (){
+                              _selectDate(context);
+                            },
+                            child: IgnorePointer(
+                              child: TextFormField(
+                                decoration: InputDecoration(
+                                    hintStyle: TextStyle(color: Palette.lightBlue),
+                                    hintText: 'Restore from date',
+                                    focusedBorder: UnderlineInputBorder(
+                                        borderSide: BorderSide(
+                                            color: Palette.lightGrey,
+                                            width: 2.0
+                                        )
+                                    ),
+                                    enabledBorder: UnderlineInputBorder(
+                                        borderSide: BorderSide(
+                                            color: Palette.lightGrey,
+                                            width: 2.0
+                                        )
+                                    )
                                 ),
+                                controller: _controller,
+                                validator: (value){
+                                  return null;
+                                },
                               ),
                             ),
-                            TextFormField(
-                              decoration: InputDecoration(
-                                  hintStyle: TextStyle(color: Palette.lightBlue),
-                                  hintText: 'Seed',
-                                  focusedBorder: UnderlineInputBorder(
-                                      borderSide: BorderSide(
-                                          color: Palette.lightGrey,
-                                          width: 2.0
-                                      )
-                                  ),
-                                  enabledBorder: UnderlineInputBorder(
-                                      borderSide: BorderSide(
-                                          color: Palette.lightGrey,
-                                          width: 2.0
-                                      )
-                                  )
-                              ),
-                              validator: (value){
-                                return null;
-                              },
-                            ),
-                          ],
-                        ),
+                          ),
+                        )
                       )
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Flexible(
+                        child: Container(
+                          padding: EdgeInsets.only(top: 20.0),
+                          child: TextFormField(
+                            decoration: InputDecoration(
+                                hintStyle: TextStyle(color: Palette.lightBlue),
+                                hintText: 'Seed',
+                                focusedBorder: UnderlineInputBorder(
+                                    borderSide: BorderSide(
+                                        color: Palette.lightGrey,
+                                        width: 2.0
+                                    )
+                                ),
+                                enabledBorder: UnderlineInputBorder(
+                                    borderSide: BorderSide(
+                                        color: Palette.lightGrey,
+                                        width: 2.0
+                                    )
+                                )
+                            ),
+                            validator: (value){
+                              return null;
+                            },
+                          ),
+                        )
+                      )
+                    ],
                   ),
                   Flexible(
-                      flex: 2,
                       child: Container(
                         alignment: Alignment.bottomCenter,
                         child: PrimaryButton(
