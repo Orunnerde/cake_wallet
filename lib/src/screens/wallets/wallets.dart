914bd7cdd50fb913933c3455ebc4b013e1cc79c3
Oleksandr Sobol <dr.alexander.sobol@gmail.com>
2019-08-21 12:17:42 +0300

    CWA-17 | Created wallets screen

---
+++ lib/src/screens/wallets/wallets.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class Wallets extends StatefulWidget{
+
+  @override
+  createState() => _WalletsState();
+
+}
+
+class _WalletsState extends State<Wallets>{
+
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+
+}
