c9268f9f921dbab011acebc6ae031e1ffc3370db
Oleksandr Sobol <dr.alexander.sobol@gmail.com>
2019-10-22 20:49:07 +0300

    CWA-71 | Added USD, XMR, Subaddress name, Node port, Node address, Contact name validation

--- lib/src/stores/validation/validation_store.dart
+++ lib/src/stores/validation/validation_store.dart
@@ -9,36 +9,89 @@ abstract class ValidationStoreBase with Store {
   @observable
   bool isValidate;
 
-  void _validate(String value, String p) {
+  bool _validate(String value, String p) {
     RegExp regExp = new RegExp(p);
     if (regExp.hasMatch(value))
-      isValidate = true;
+      return true;
     else
-      isValidate = false;
+      return false;
   }
 
   @action
   void validateWalletName(String value) {
     String p = '^[a-zA-Z0-9_]{1,10}\$';
-    _validate(value, p);
+    isValidate = _validate(value, p);
   }
 
   @action
   void validateKeys(String value) {
     String p = '^[a-fA-F0-9]{64}\$';
-    _validate(value, p);
+    isValidate = _validate(value, p);
   }
 
   @action
   void validateAddress(String value) {
-    String p = 'xmr|btc|eth|ltc|bch|dash';
-    _validate(value, p);
+    String p = 'XMR|BTC|ETH|LTC|BCH|DASH';
+    isValidate = _validate(value, p);
   }
 
   @action
   void validatePaymentID(String value) {
     String p = '^[a-fA-F0-9]{16,64}\$';
-    _validate(value, p);
+    isValidate = _validate(value, p);
+  }
+
+  @action
+  void validateUSD(String value, double totalXMR, double rateXMRUSD, double fee) {
+    const double minValue = 0.01;
+    final double maxValue = totalXMR*rateXMRUSD - fee;
+    String p = '^[0-9]*.[0-9]+';
+    if (_validate(value, p)) {
+      try {
+        double dValue = double.parse(value);
+        isValidate = (dValue >= minValue && dValue <= maxValue);
+      } catch (e) {
+        isValidate = false;
+      }
+    } else isValidate = false;
+  }
+
+  @action
+  void validateXMR(String value, double maxAvailable, double fee) {
+    const double maxValue = 18446744.073709551616;
+    String p = '^[0-9]*.[0-9]{12}';
+    if (_validate(value, p)) {
+      try {
+        double dValue = double.parse(value) + fee;
+        isValidate = (dValue <= maxAvailable && dValue <= maxValue);
+      } catch (e) {
+        isValidate = false;
+      }
+    } else isValidate = false;
+  }
+
+  @action
+  void validateSubaddressName(String value) {
+    String p = '''^[^`,'"]{1,20}\$''';
+    isValidate = _validate(value, p);
+  }
+
+  @action
+  void validateNodePort(String value) {
+    String p = '^[0-9]{1,5}';
+    isValidate = _validate(value, p);
+  }
+
+  @action
+  void validateNodeAddress(String value) {
+    String p = '^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\$';
+    isValidate = _validate(value, p);
+  }
+
+  @action
+  void validateContactName(String value) {
+    String p = '''^[^`,'"]{1,32}\$''';
+    isValidate = _validate(value, p);
   }
 
 }
