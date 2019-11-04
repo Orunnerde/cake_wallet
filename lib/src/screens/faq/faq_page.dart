78b889ad8dd54a9c6c46ab71a556020cfa068a08
Oleksandr Sobol <dr.alexander.sobol@gmail.com>
2019-11-04 21:54:50 +0200

    CWA-73 | Fixed faq page. Questions and answers relocated to json file

--- lib/src/screens/faq/faq_page.dart
+++ lib/src/screens/faq/faq_page.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:cake_wallet/src/screens/base_page.dart';
-import 'package:cake_wallet/src/screens/faq/faq_items.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:cake_wallet/theme_changer.dart';
 import 'package:cake_wallet/themes.dart';
 import 'package:cake_wallet/palette.dart';
+import 'dart:convert';
 
 class FaqPage extends BasePage {
   String get title => 'FAQ';
@@ -14,42 +15,49 @@ class FaqPage extends BasePage {
     ThemeChanger _themeChanger = Provider.of<ThemeChanger>(context);
     bool _isDarkTheme = (_themeChanger.getTheme() == Themes.darkTheme);
 
-    return ListView.separated(
-      itemBuilder: (BuildContext context, int index) {
-        final itemTitle = FaqItems.map.keys.elementAt(index);
-        final itemChild = FaqItems.map.values.elementAt(index);
+    return FutureBuilder(
+      builder: (context, snapshot) {
+        var faqItems = json.decode(snapshot.data.toString());
 
-        return ExpansionTile(
-          title: Text(
-            itemTitle
-          ),
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
+        return ListView.separated(
+          itemBuilder: (BuildContext context, int index) {
+            final itemTitle = faqItems[index]["question"];
+            final itemChild = faqItems[index]["answer"];
+
+            return ExpansionTile(
+              title: Text(
+                itemTitle
+              ),
               children: <Widget>[
-                Expanded(
-                  child: Container(
-                    padding: EdgeInsets.only(
-                      left: 15.0,
-                      right: 15.0
-                    ),
-                    child: Text(
-                      itemChild,
-                    ),
-                  )
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Expanded(
+                        child: Container(
+                          padding: EdgeInsets.only(
+                              left: 15.0,
+                              right: 15.0
+                          ),
+                          child: Text(
+                            itemChild,
+                          ),
+                        )
+                    )
+                  ],
                 )
               ],
-            )
-          ],
+            );
+          },
+          separatorBuilder: (_, __) => Divider(
+            color: _isDarkTheme
+                ? PaletteDark.darkThemeGreyWithOpacity
+                : Palette.lightGrey,
+            height: 1.0,
+          ),
+          itemCount: faqItems == null ? 0 : faqItems.length,
         );
       },
-      separatorBuilder: (_, __) => Divider(
-        color: _isDarkTheme
-            ? PaletteDark.darkThemeGreyWithOpacity
-            : Palette.lightGrey,
-        height: 1.0,
-      ),
-      itemCount: FaqItems.map.length == null ? 0 : FaqItems.map.length,
+      future: rootBundle.loadString('assets/faq/faq.json'),
     );
   }
 
