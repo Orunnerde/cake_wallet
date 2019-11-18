5007a6ce6b45913310f263c938205b985a4230cb
Oleksandr Sobol <dr.alexander.sobol@gmail.com>
2019-11-18 20:43:09 +0200

    CWA-120 | Reworked seed page

--- lib/src/screens/seed/seed_page.dart
+++ lib/src/screens/seed/seed_page.dart
@@ -13,7 +13,6 @@ import 'package:cake_wallet/themes.dart';
 
 class SeedPage extends BasePage {
   static final image = Image.asset('assets/images/seed_image.png');
-  bool get isModalBackButton => true;
   String get title => 'Seed';
 
   final VoidCallback onCloseCallback;
@@ -24,146 +23,161 @@ class SeedPage extends BasePage {
       onCloseCallback != null ? onCloseCallback() : Navigator.of(context).pop();
 
   @override
+  Widget leading(BuildContext context) {
+    final _closeButtonImage = Image.asset('assets/images/close_button.png');
+    final _closeButtonImageDarkTheme =
+    Image.asset('assets/images/close_button_dark_theme.png');
+
+    ThemeChanger _themeChanger = Provider.of<ThemeChanger>(context);
+    Image _closeButton = _themeChanger.getTheme() == Themes.darkTheme ?
+    _closeButtonImageDarkTheme
+    : _closeButtonImage;
+
+    return SizedBox(
+      height: 37,
+      width:  37,
+      child: ButtonTheme(
+        minWidth: double.minPositive,
+        child: onCloseCallback != null ?
+        Offstage()
+        : FlatButton(
+            highlightColor: Colors.transparent,
+            splashColor: Colors.transparent,
+            padding: EdgeInsets.all(0),
+            onPressed: () => onClose(context),
+            child: _closeButton),
+      ),
+    );
+  }
+
+  @override
   Widget body(BuildContext context) {
     final walletSeedStore = Provider.of<WalletSeedStore>(context);
     String _seed;
 
     ThemeChanger _themeChanger = Provider.of<ThemeChanger>(context);
-    bool _isDarkTheme;
+    bool _isDarkTheme = _themeChanger.getTheme() == Themes.darkTheme;
 
-    if (_themeChanger.getTheme() == Themes.darkTheme)
-      _isDarkTheme = true;
-    else
-      _isDarkTheme = false;
-
-    return Form(
-        child: GestureDetector(
-      onTap: () {
-        SystemChannels.textInput.invokeMethod('TextInput.hide');
-      },
-      child: Center(
-        child: Container(
-          padding: EdgeInsets.only(left: 30.0, right: 30.0),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              image,
-              Container(
-                margin: EdgeInsets.only(
-                    left: 30.0, top: 10.0, right: 30.0, bottom: 20.0),
-                child: Observer(builder: (_) {
-                  _seed = walletSeedStore.seed;
-                  return Column(
-                    children: <Widget>[
-                      Row(
+    return Container(
+      padding: EdgeInsets.all(30.0),
+      child: Column(
+        children: <Widget>[
+          Expanded(
+            child: Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  image,
+                  Container(
+                    margin: EdgeInsets.only(
+                        left: 30.0, top: 10.0, right: 30.0),
+                    child: Observer(builder: (_) {
+                      _seed = walletSeedStore.seed;
+                      return Column(
                         children: <Widget>[
-                          Expanded(
-                              child: Container(
-                            decoration: BoxDecoration(
-                                border: Border(
-                                    bottom: BorderSide(
-                                        width: 1.0,
-                                        color: _isDarkTheme
-                                            ? PaletteDark.darkThemeDarkGrey
-                                            : Palette.lightGrey))),
-                            padding: EdgeInsets.only(bottom: 20.0),
-                            margin: EdgeInsets.only(bottom: 10.0),
-                            child: Text(
-                              walletSeedStore.name,
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
-                                  fontSize: 18.0,
-                                  color: _isDarkTheme
-                                      ? Palette.wildDarkBlue
-                                      : Colors.black),
-                            ),
-                          ))
-                        ],
-                      ),
-                      SizedBox(
-                        height: 10.0,
-                      ),
-                      Text(
-                        walletSeedStore.seed,
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontSize: 14.0,
-                            color: _isDarkTheme
-                                ? PaletteDark.darkThemeTitle
-                                : Colors.black),
-                      )
-                    ],
-                  );
-                }),
-              ),
-              Container(
-                margin: EdgeInsets.only(top: 30.0, bottom: 30.0),
-                child: Column(
-                  children: <Widget>[
-                    /*Text(
-                      'Please share, copy, or write down your seed. The seed '
-                      'is used to recover your wallet. This is your PRIVATE '
-                      'seed. DO NOT SHARE this seed with other people!',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: Palette.lightBlue,
-                      ),
-                    ),*/
-                    Container(
-                      child: Row(
-                        children: <Widget>[
-                          Flexible(
-                              child: Container(
-                            padding: EdgeInsets.only(right: 8.0),
-                            child: PrimaryButton(
-                                onPressed: () => Share.text(
-                                    'Share seed', _seed, 'text/plain'),
-                                color: _isDarkTheme
-                                    ? PaletteDark.darkThemePurpleButton
-                                    : Palette.purple,
-                                borderColor: _isDarkTheme
-                                    ? PaletteDark.darkThemePurpleButtonBorder
-                                    : Palette.deepPink,
-                                text: 'Save'),
-                          )),
-                          Flexible(
-                              child: Container(
-                                  padding: EdgeInsets.only(left: 8.0),
-                                  child: Builder(
-                                    builder: (context) => PrimaryButton(
-                                      onPressed: () {
-                                        Clipboard.setData(
-                                            ClipboardData(text: _seed));
-                                        Scaffold.of(context).showSnackBar(
-                                          SnackBar(
-                                            content:
-                                                Text('Copied to Clipboard'),
-                                            backgroundColor: Colors.green,
-                                            duration:
-                                                Duration(milliseconds: 1500),
-                                          ),
-                                        );
-                                      },
-                                      text: 'Copy',
-                                      color: _isDarkTheme
-                                          ? PaletteDark.darkThemeBlueButton
-                                          : Palette.brightBlue,
-                                      borderColor: _isDarkTheme
-                                          ? PaletteDark
-                                              .darkThemeBlueButtonBorder
-                                          : Palette.cloudySky,
+                          Row(
+                            children: <Widget>[
+                              Expanded(
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                        border: Border(
+                                            bottom: BorderSide(
+                                                width: 1.0,
+                                                color: _isDarkTheme
+                                                    ? PaletteDark.darkThemeDarkGrey
+                                                    : Palette.lightGrey))),
+                                    padding: EdgeInsets.only(bottom: 20.0),
+                                    margin: EdgeInsets.only(bottom: 10.0),
+                                    child: Text(
+                                      walletSeedStore.name,
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                          fontSize: 18.0,
+                                          color: _isDarkTheme
+                                              ? Palette.wildDarkBlue
+                                              : Colors.black),
                                     ),
-                                  )))
+                                  ))
+                            ],
+                          ),
+                          SizedBox(
+                            height: 10.0,
+                          ),
+                          Text(
+                            walletSeedStore.seed,
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                fontSize: 14.0,
+                                color: _isDarkTheme
+                                    ? PaletteDark.darkThemeTitle
+                                    : Colors.black),
+                          )
                         ],
-                      ),
-                    )
-                  ],
-                ),
-              )
-            ],
+                      );
+                    }),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 30.0),
+                    child: Row(
+                      children: <Widget>[
+                        Flexible(
+                            child: Container(
+                              padding: EdgeInsets.only(right: 8.0),
+                              child: PrimaryButton(
+                                  onPressed: () => Share.text(
+                                      'Share seed', _seed, 'text/plain'),
+                                  color: _isDarkTheme
+                                      ? PaletteDark.darkThemePurpleButton
+                                      : Palette.purple,
+                                  borderColor: _isDarkTheme
+                                      ? PaletteDark.darkThemePurpleButtonBorder
+                                      : Palette.deepPink,
+                                  text: 'Save'),
+                            )),
+                        Flexible(
+                            child: Container(
+                                padding: EdgeInsets.only(left: 8.0),
+                                child: Builder(
+                                  builder: (context) => PrimaryButton(
+                                    onPressed: () {
+                                      Clipboard.setData(
+                                          ClipboardData(text: _seed));
+                                      Scaffold.of(context).showSnackBar(
+                                        SnackBar(
+                                          content:
+                                          Text('Copied to Clipboard'),
+                                          backgroundColor: Colors.green,
+                                          duration:
+                                          Duration(milliseconds: 1500),
+                                        ),
+                                      );
+                                    },
+                                    text: 'Copy',
+                                    color: _isDarkTheme
+                                        ? PaletteDark.darkThemeBlueButton
+                                        : Palette.brightBlue,
+                                    borderColor: _isDarkTheme
+                                        ? PaletteDark
+                                        .darkThemeBlueButtonBorder
+                                        : Palette.cloudySky,
+                                  ),
+                                )))
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
           ),
-        ),
+          onCloseCallback != null ?
+          PrimaryButton(
+              onPressed: () => onClose(context),
+              text: 'Next',
+              color: Palette.lightGrey,
+              borderColor: Palette.darkGrey)
+          : Offstage()
+        ],
       ),
-    ));
+    );
   }
 }
