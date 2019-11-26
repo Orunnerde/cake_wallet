35d8322415a1159df652c6b27a828a9d03143519
Oleksandr Sobol <dr.alexander.sobol@gmail.com>
2019-11-26 09:50:21 +0200

    CWA-80 | Added http client

--- lib/src/domain/common/node.dart
+++ lib/src/domain/common/node.dart
@@ -1,7 +1,16 @@
+import 'dart:io';
+
 import 'package:flutter/foundation.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import 'dart:io';
+/*import 'package:http/http.dart' show BaseClient;
+import 'dart:io'
+    show HttpClient, HttpClientBasicCredentials, HttpClientCredentials;
+import 'dart:async' show Future;
+
+import 'package:http/io_client.dart';*/
+
+import 'package:http_auth/http_auth.dart';
 
 class Node {
   final int id;
@@ -33,11 +42,41 @@ class Node {
     String body = json.encode({"jsonrpc":"2.0","id":"0","method":"get_info"});
 
     if (login != null && password != null) {
-      String basicAuth =
-          'Basic ' + base64Encode(utf8.encode('$login:$password'));
-      print("AUTH = $basicAuth");
+      /*String basicAuth = 'Basic ' + base64Encode(utf8.encode('$login:$password'));
       headers.addAll({HttpHeaders.authorizationHeader: basicAuth});
-      print("HEADERS = ${headers.toString()}");
+      print("HEADERS = ${headers.toString()}");*/
+      /*final httpAuth = createBasicAuthenticationIoHttpClient(login, password);
+      response = await httpAuth.post(url.toString(), headers: headers, body: body);*/
+
+      HttpClient client = new HttpClient();
+      client.addCredentials(
+          Uri.parse(url.toString()),
+          'realm',
+          new HttpClientBasicCredentials(login, password)
+      );
+
+      client.getUrl(Uri.parse(url.toString()))
+          .then((HttpClientRequest req) {
+        req.headers
+          ..add(HttpHeaders.ACCEPT, 'application/json')
+          ..add(HttpHeaders.CONTENT_TYPE, 'application/json');
+
+        return req.close();
+      })
+          .then((HttpClientResponse res) {
+        print("AUTHORIZATION: ${res.statusCode}");
+        client.close();
+        return true;
+      });
+
+      /*var client = new DigestAuthClient('$login', '$password');
+      var res = await client.post(url.toString(), headers: headers, body: body);
+      var resBody = json.decode(res.body);
+      var isOffline = resBody["result"]["offline"];
+      print("URL = $url");
+      print("Is offline $uri: $isOffline");
+      print(resBody);
+      return isOffline;*/
     }
 
     var response = await http.post(url.toString(), headers: headers, body: body);
@@ -47,6 +86,26 @@ class Node {
     print("Is offline $uri: $isOffline");
     print(resBody);
     return isOffline;
-
   }
+
 }
+
+  /*HttpAuthenticationCallback _basicAuthenticationCallback(
+      HttpClient client, HttpClientCredentials credentials) =>
+          (Uri uri, String scheme, String realm) {
+        client.addCredentials(uri, realm, credentials);
+        return new Future.value(true);
+      };
+
+  BaseClient createBasicAuthenticationIoHttpClient(
+      String userName, String password) {
+    final credentials = new HttpClientBasicCredentials(userName, password);
+
+    final client = new HttpClient();
+    client.authenticate = _basicAuthenticationCallback(client, credentials);
+    return new IOClient(client);
+  }*/
+
+
+/*typedef Future<bool> HttpAuthenticationCallback(
+    Uri uri, String scheme, String realm);*/
